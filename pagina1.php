fe3b3b0b2868bd8f57613842f78ffecd3bba4d5b
Gui1304 <130668551+Gui1304@users.noreply.github.com>
2023-05-12 15:32:16 -0300

    commit do Guilhermão

--- pagina1.php
+++ pagina1.php
@@ -9,9 +9,16 @@
 <body>
     <?php
 
-        echo "<p>Boa tarde à todos!</p>"
+        //Declaração de variáveis
+        $hora = date("H");
     
+        if($hora <12) {
+            echo "<p> " .$hora. "hs. Bom dia a todos!<p>";
+        }else if ($hora < 18) {
+          echo "<p> " .$hora. "hs. Boa tarde a todos!</p>";
+        } else {
+                echo "<p> " .$hora. "hs. Boa noite a todos!</p>";
+        }
     ?>
-    
 </body>
 </html>
